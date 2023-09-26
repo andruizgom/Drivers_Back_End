368a9515f8e077453156a3b6227583350c579f58
Anderson <100323069+andruizgom@users.noreply.github.com>
2023-09-26 01:45:08 -0500

    Update

--- src/db.js
+++ src/db.js
@@ -4,12 +4,13 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false,
   native: false,
+  dialectOptions: {ssl: {require: true}}
 });
 const basename = path.basename(__filename);
 
